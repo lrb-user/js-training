3f3166c5dc87953f657087164538c3e65e340dd1
lrb-user <ngy1229@naver.com>
2024-12-14 11:08:03 +0900

    Merge branch 'main' of https://github.com/lrb-user/js-training
    # Conflicts:
    #	10/10-1.js

--- 10/10-1.js
+++ 10/10-1.js
@@ -1,8 +1,8 @@
-var person2 = {
+var person = {
   name: "Lee",
   sayHellow: function () {
     console.log(`Hello! My name is ${this.name}`);
   },
 };
-console.log(typeof person2);
-console.log(person2);
+console.log(typeof person);
+console.log(person);
