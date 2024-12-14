15f979f1cfd5956b8dceeea3132f96fb34e558ad
lrb-user <ngy1229@naver.com>
2024-12-14 11:02:06 +0900

    feat:커밋

--- 10/10-1.js
+++ 10/10-1.js
@@ -1,8 +1,8 @@
-var person = {
-    name:"Lee",
-    sayHellow:function(){
-        console.log(`Hello! My name is ${this.name}`);
-    }
+var person2 = {
+  name: "Lee",
+  sayHellow: function () {
+    console.log(`Hello! My name is ${this.name}`);
+  },
 };
-console.log(typeof person);
-console.log(person);
+console.log(typeof person2);
+console.log(person2);
