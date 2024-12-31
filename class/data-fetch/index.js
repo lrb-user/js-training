b881540e24669dc2454a7bdb8f06c03496e546e0
lrb-user <ngy1229@naver.com>
2024-12-31 20:37:44 +0900

    feat:data-fetch

---
+++ class/data-fetch/index.js
@@ -0,0 +1,8 @@
+// https://jsonplaceholder.typicode.com/photos
+// {
+// "albumId": 1,
+// "id": 2,
+// "title": "reprehenderit est deserunt velit ipsam",
+// "url": "https://via.placeholder.com/600/771796",
+// "thumbnailUrl": "https://via.placeholder.com/150/771796"
+// }
